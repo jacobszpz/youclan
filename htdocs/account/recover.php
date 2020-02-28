b643841c47975ec84ce62b35d139df9816b4429b
We Are Magic <wearemagic@protonmail.com>
2020-02-28 11:44:15 +0000

    test

--- htdocs/account/recover.php
+++ htdocs/account/recover.php
@@ -17,7 +17,7 @@
   // To Test Logged In Redirect
   // $loggedIn = TRUE;
 
-  $debuggingActivated = FALSE;
+  $debuggingActivated = TRUE;
   $phpErrorMessage = "Debugging Activated<br>";
 
   // Do not show page if user is already logged in
