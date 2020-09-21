0acf8d8e2805da778187cedf96eb7fabd6db0640
We Are Magic <wearemagic@protonmail.com>
2020-09-21 15:30:50 +0100

    Testing API

--- htdocs/4wc_api/quiz.php
+++ htdocs/4wc_api/quiz.php
@@ -1,5 +1,16 @@
 <?php
   $file_root = $_SERVER['DOCUMENT_ROOT'] . "/";
   $quiz = json_decode(file_get_contents($file_root . '4wc_api/json/questions_01.json'), TRUE);
+
+  $randomise = filter_input(INPUT_GET, 'randomise', FILTER_VALIDATE_BOOLEAN);
+
+  if ($randomise) {
+    foreach ($quiz as $question) {
+      $correct = $quiz['choices'][$quiz['correct']];
+      shuffle($quiz['choices']);
+      $quiz['correct'] = array_search($correct, $quiz['choices'], TRUE);
+    }
+  }
+
   echo json_encode($quiz);
 ?>
