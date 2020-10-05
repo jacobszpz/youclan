9e60af66450de9ab87507c49a3943a5d01b6752a
jacobszpz <jacobszpz@protonmail.com>
2020-10-05 22:34:27 +0100

    Github upload

--- htdocs/4wc_api/quiz.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $file_root = $_SERVER['DOCUMENT_ROOT'] . "/";
-  $quiz = json_decode(file_get_contents($file_root . '4wc_api/json/questions_01.json'), TRUE);
-
-  $randomise = filter_input(INPUT_GET, 'randomise', FILTER_VALIDATE_BOOLEAN);
-
-  echo json_encode($quiz);
-?>
